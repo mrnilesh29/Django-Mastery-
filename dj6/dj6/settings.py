ab7fc49a8c8ac7f9f5d0e40faba0ca7abb0f4798
Nilesh Shah <mr.nilesh29@gmail.com>
2026-04-09 15:06:51 +0530

    update seeting.py and base directory

--- dj6/dj6/settings.py
+++ dj6/dj6/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'type', 
+    'product'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'dj6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dj6/product/urls.py
+++ dj6/product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path("product" , views.product ,name="product")
+    path("product/" , views.product ,name="product")
 ]
