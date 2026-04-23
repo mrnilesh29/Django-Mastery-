9711848186c07e95c04bf8b850001e792b1f2810
Nilesh Shah <mr.nilesh29@gmail.com>
2026-04-23 09:13:58 +0530

    basic model form

--- dj15/Student/urls.py
+++ dj15/Student/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('student1' , views.student1 , name="student"), 
     path('studentlist' , views.student_list , name="studentlist"),
+    path('studentedit/<int:id>' , views.student_edit , name="Student_edit"), 
     
 ]
 
--- dj15/Student/views.py
+++ dj15/Student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import Student_Form
 from django.http import HttpResponse
 from .models import Student_details
@@ -20,6 +20,29 @@ def student1(request) :
 def student_list(request): 
     student = Student_details.objects.all()
     return render(request , 'student_list.html' , {'student' : student})
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student_details
+from .forms import Student_Form
+
+def student_edit(request, id):   # 👈 id lena zaruri hai
+    student = get_object_or_404(Student_details, id=id)  # ✔️ model use karo
+
+    if request.method == "POST":
+        form = Student_Form(request.POST, instance=student)  # ✔️ instance pass
+
+        if form.is_valid():
+            form.save()
+            return redirect('studentlist')
+
+    else:
+        form = Student_Form(instance=student)  # ✔️ existing data show
+
+    return render(request, "studentedit.html", {'form': form})
+    
+        
+        
+    
 
     
 
